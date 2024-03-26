4d0bb74645fa8c440698bd368e1f417e429363e4
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2024-03-26 15:32:17 +0500

    Update ReviewCard.jsx

--- src/ReviewCard.jsx
+++ src/ReviewCard.jsx
@@ -1,31 +1,28 @@
 import { faLock, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
+
 const ReviewCard = ({ imageSrc = "/orange.jpg", title = "Organic Orange" }) => {
   return (
-    <div className="relative flex justify-between items-center w-fit bg-gray-200 border rounded-lg p-4 mx-2 my-2">
-      <div className="flex-shrink-0 w-36 h-36 border rounded-full overflow-hidden">
-        <img
-          className="object-cover w-full h-full"
-          src={imageSrc}
-          alt="Orange"
-        />
+    <div className="flex flex-col justify-between bg-gray-200 border rounded-lg p-4 mx-2 my-2 md:flex-row md:p-6">
+      <div className="flex-shrink-0 w-36 h-36 md:w-48 md:h-48 border rounded-full overflow-hidden">
+        <img className="object-cover w-full h-full" src={imageSrc} alt="Orange" />
       </div>
-      <div className="ml-4">
-        <h4 className="text-2xl opacity-80 font-semibold">{title}</h4>
-        <div className="flex flex-col items-center">
-          <p className="text-green-500 my-4">
+      <div className="flex flex-col md:ml-4 md:mt-0 md:text-left">
+        <h4 className="text-lg md:text-xl lg:text-2xl opacity-80 font-semibold">{title}</h4>
+        <div className="flex flex-col items-center md:flex-row md:items-center">
+          <p className="text-sm md:text-base font-semibold text-green-500 mt-2 md:mt-4 md:mr-4">
             <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} />{" "}
             <FontAwesomeIcon icon={faStar} /> <FontAwesomeIcon icon={faStar} />
             <FontAwesomeIcon icon={faStar} />
           </p>
-          <p className="text-xl font-semibold text-gray-500">3.12 $</p>
+          <p className="text-sm md:text-base font-semibold text-gray-500 mt-2 md:mt-4">$3.12</p>
         </div>
         <a
-          className="text-2xl block mt-2 text-green-600 border border-orange-400 hover:bg-orange-400 hover:text-white px-4 py-3 rounded-full"
+          className="text-sm md:text-base block mt-2 md:mt-4 text-green-600 border border-orange-400 hover:bg-orange-400 hover:text-white px-4 py-3 rounded-full"
           href="#"
         >
-          <FontAwesomeIcon icon={faLock} /> Add to Cart
+          <FontAwesomeIcon icon={faLock} className="mr-2" /> Add to Cart
         </a>
       </div>
     </div>
