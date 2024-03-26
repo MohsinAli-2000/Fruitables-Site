444d1b14786dccf916964512edc4c4c81fdad818
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2024-03-26 15:17:24 +0500

    Update Acheivements.jsx

--- src/Acheivements.jsx
+++ src/Acheivements.jsx
@@ -1,15 +1,16 @@
-import AcheivementCard from "./AcheivementCard"
-
+import AcheivementCard from "./AcheivementCard";
 
 const Acheivements = () => {
   return (
-    <div className="flex flex-wrap justify-center items-center bg-gray-200 my-8 mx-16 rounded-lg">
-      <AcheivementCard/>
-      <AcheivementCard title="QUALITY OF SERVICE" Rate="99%"/>
-      <AcheivementCard title="QUALITY CERTIFICATES" Rate="33"/>
-      <AcheivementCard title="AVAILABLE PRODUCTS" Rate="789"/>
+    <div className="flex flex-wrap justify-center items-center bg-gray-200 my-8 mx-4 md:mx-16 rounded-lg">
+      <div className="flex flex-wrap justify-center items-center w-full md:w-auto">
+        <AcheivementCard />
+        <AcheivementCard title="QUALITY OF SERVICE" Rate="99%" />
+        <AcheivementCard title="QUALITY CERTIFICATES" Rate="33" />
+        <AcheivementCard title="AVAILABLE PRODUCTS" Rate="789" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Acheivements
+export default Acheivements;
