58938a42f0ce1c6bb820378314b7bb5976ce16a4
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2024-03-26 15:17:58 +0500

    Update AcheivementCard.jsx

--- src/AcheivementCard.jsx
+++ src/AcheivementCard.jsx
@@ -4,15 +4,10 @@ import PropTypes from "prop-types";
 
 const AcheivementCard = ({ title = "SATISFIED CUSTOMERS", Rate = "1963" }) => {
   return (
-    <div className="flex flex-col w-fit max-w-48 justify-center items-center rounded-lg mx-2 my-2 px-4 py-6 bg-white">
-      <FontAwesomeIcon
-        className="my-4 text-6xl text-orange-400"
-        icon={faUsers}
-      />
-      <h4 className="my-4 text-3xl break-words font-semibold text-green-600">
-        {title}
-      </h4>
-      <span className="my-4 text-3xl font-bold opacity-70">{Rate}</span>
+    <div className="flex flex-col items-center justify-center mx-2 my-2 p-4 bg-white rounded-lg shadow-lg">
+      <FontAwesomeIcon className="text-4xl text-orange-400" icon={faUsers} />
+      <h4 className="mt-4 text-lg md:text-xl lg:text-2xl font-semibold text-green-600">{title}</h4>
+      <span className="mt-4 text-lg md:text-xl lg:text-2xl font-bold opacity-70">{Rate}</span>
     </div>
   );
 };
